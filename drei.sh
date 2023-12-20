48344ba67df5c227a43d7524333f0c262d0f1c21
legdna <legdna@local.lan>
2023-12-20 18:22:41 +0100

    refactor: NVIDIA experimental support added, change in the way important messages are displayed and OpenSUSE support added

--- drei.sh
+++ drei.sh
@@ -3,7 +3,11 @@ set -e
 
 test_sudo=$(which sudo)
 if [[ $? -ne 0 ]]; then
-    printf "\e[31;1mSudo n'est pas installé sur votre machine !\e[0m\n"
+    printf "\n
+		\e[41;1m                                               \e[0m
+		\e[41;1m  Sudo n'est pas installé sur votre machine !  \e[0m
+		\e[41;1m                                               \e[0m
+	\n\n"
     exit 1
 fi
 
@@ -22,7 +26,7 @@ fi
 # "underlined" "\e[4m"
 
 function title {
-	printf "\e[1m                                     
+	printf "\e[35;1m
 		     _____          ___           ___                 
 		    /  /::\        /  /\         /  /\        ___     
 		   /  /:/\:\      /  /::\       /  /:/_      /  /\    
@@ -34,7 +38,7 @@ function title {
 		    \  \::/      \  \:\        \  \:\/:/       /__/:/ 
 		     \__\/        \  \:\        \  \::/        \__\/  
 		                   \__\/         \__\/                
-	
+	\e[0;1m
 		 by legdna                                    rev.a0.1
 	
 	
@@ -43,11 +47,19 @@ function title {
 
 # Message d'erreur lors d'une syntaxe douteuse
 function error_syntax {
-	printf "\e[31;1m
-		-----------------------
-		| Erreur de syntaxe ! |
-		-----------------------
-		\e[0m\n\n"
+	printf "\n
+		\e[41;1m                       \e[0m
+		\e[41;1m  Erreur de syntaxe !  \e[0m
+		\e[41;1m                       \e[0m
+	\n\n"
+}
+
+function root_passwd {
+	printf "\n
+		\e[42;1m                                                                                                \e[0m
+		\e[42;1m  Entrez votre mot de passe utilisateur pour installer les dépendances nécessaire au programme  \e[0m
+		\e[42;1m                                                                                                \e[0m
+  	\n\n"
 }
 
 clear
@@ -60,14 +72,31 @@ while [[ $yes_or_no != "o" ]] && [[ $yes_or_no != "O" ]]; do
 			error_syntax
 		fi
 		printf "\e[36;1m  Sur quelle distribution Linux êtes-vous ?\e[0m\n"
-		printf "\n	1. \e[31mDebian\e[0m ou équivalent (Ubuntu, Linux Mint, ...)"
-		printf "\n	2. \e[34mFedora\e[0m ou équivalent (Bazzite, ...)"
-		printf "\n	3. \e[36mArch Linux\e[0m ou équivalent (Manjaro, Endeavour, ...)\n"
+		printf "\n	1. \e[31;1mDebian\e[0m ou équivalent (Ubuntu, Linux Mint, ...)"
+		printf "\n	2. \e[34;1mFedora\e[0m ou équivalent (Bazzite, ...)"
+		printf "\n	3. \e[36;1mArch Linux\e[0m ou équivalent (Manjaro, Endeavour, ...)"
+		printf "\n	4. \e[32;1mOpenSUSE\e[0m ou équivalent (SUSE, ...)\n"
 		printf "\n\e[33;1m  -> \e[0m"; read distro
-		if [[ $distro =~ ^[1-3]$ ]]; then
+		if [[ $distro =~ ^[1-4]$ ]]; then
 			distro_choice=1
 		else
 			distro_choice=-1
+		fi
+		clear
+	done
+
+	nvidia_choice=0
+	while [[ $nvidia_choice == 0 ]] || [[ $nvidia_choice == -1 ]]; do
+		if [[ $nvidia_choice == -1 ]]; then
+			error_syntax
+		fi
+		printf "\n\e[36;1m  Avez-vous une carte NVIDIA ? o/N : \e[0m"; read nvidia_check
+		if [[ $nvidia_check == "o" ]] || [[ $nvidia_check == "O" ]]; then
+			nvidia_choice=2
+		elif [[ $nvidia_check == "n" ]] || [[ $nvidia_check == "N" ]] || [[ $nvidia_check == "" ]]; then
+			nvidia_choice=1
+		else
+			nvidia_choice=-1
 		fi
 		clear
 	done
@@ -77,11 +106,7 @@ while [[ $yes_or_no != "o" ]] && [[ $yes_or_no != "O" ]]; do
 		read yes_or_no
 		if [[ $yes_or_no == "o" ]] || [[ $yes_or_no == "O" ]]; then
 			clear
-			printf "\n\e[32;1m
-		------------------------------------------------------------------------------------------------
-		| Entrez votre mot de passe utilisateur pour installer les dépendances nécessaire au programme |
-		------------------------------------------------------------------------------------------------
-  			\e[0m\n\n"
+			root_passwd
 			sudo apt update -y && sudo apt upgrade -y
 			sudo apt install -y distrobox podman fuse-overlayfs unzip
 		else
@@ -92,11 +117,7 @@ while [[ $yes_or_no != "o" ]] && [[ $yes_or_no != "O" ]]; do
 		read yes_or_no
 		if [[ $yes_or_no == "o" ]] || [[ $yes_or_no == "O" ]]; then
 			clear
-			printf "\n\e[32;1m
-		------------------------------------------------------------------------------------------------
-		| Entrez votre mot de passe utilisateur pour installer les dépendances nécessaire au programme |
-		------------------------------------------------------------------------------------------------
-  			\e[0m\n\n"
+			root_passwd
 			sudo dnf update -y
 			sudo dnf install -y distrobox podman fuse-overlayfs unzip
 		else
@@ -107,33 +128,49 @@ while [[ $yes_or_no != "o" ]] && [[ $yes_or_no != "O" ]]; do
 		read yes_or_no
 		if [[ $yes_or_no == "o" ]] || [[ $yes_or_no == "O" ]]; then
 			clear
-			printf "\n\e[32;1m
-		------------------------------------------------------------------------------------------------
-		| Entrez votre mot de passe utilisateur pour installer les dépendances nécessaire au programme |
-		------------------------------------------------------------------------------------------------
-  			\e[0m\n\n"
+			root_passwd
 			sudo pacman -Suy --noconfirm distrobox podman fuse-overlayfs unzip
+		else
+			clear
+		fi
+	elif [[ $distro == 4 ]]; then
+		printf "\n\e[33;1m  Êtes-vous sûr d'être sur OpenSUSE ou équivalent ? o/N : \e[0m"
+		read yes_or_no
+		if [[ $yes_or_no == "o" ]] || [[ $yes_or_no == "O" ]]; then
+			clear
+			root_passwd
+			sudo zypper update -y 
+			sudo zypper install -y distrobox podman fuse-overlayfs unzip
 		else
 			clear
 		fi
 	fi
 done
 
-distrobox-create --name drei --image fedora:38
+if [[ $nvidia_choice == 2 ]]; then
+	distrobox-create --name drei --image fedora:38 --nvidia
+else
+	distrobox-create --name drei --image fedora:38
+fi
 distrobox-enter --name drei -- sudo dnf install -y alsa-plugins-pulseaudio libxcrypt-compat xcb-util-renderutil xcb-util-wm pulseaudio-libs xcb-util xcb-util-image xcb-util-keysyms libxkbcommon-x11 libXrandr libXtst mesa-libGLU mtdev libSM libXcursor libXi libXinerama libxkbcommon libglvnd-egl libglvnd-glx libglvnd-opengl libICE librsvg2 libSM libX11 libXcursor libXext libXfixes libXi libXinerama libxkbcommon libxkbcommon-x11 libXrandr libXrender libXtst libXxf86vm mesa-libGLU mtdev pulseaudio-libs xcb-util alsa-lib apr apr-util fontconfig freetype libglvnd fuse-libs fuse rocm-opencl
 
 clear
-printf "\e[36;1m\n
-	----------------------------------------------------------------------------------------------------------------
-	| Téléchargez votre version de DaVinci Resolve sur https://www.blackmagicdesign.com/fr/products/davinciresolve |
-	----------------------------------------------------------------------------------------------------------------
+printf "\n
+	\e[42;1m                                                                                                                \e[0m
+	\e[42;1m  Téléchargez votre version de DaVinci Resolve sur https://www.blackmagicdesign.com/fr/products/davinciresolve  \e[0m
+	\e[42;1m                                                                                                                \e[0m
 \e[0m\e[33;1m
 	Une fois fait glisser déposer le fichier téléchargé ici : \e[0m"
 read davinci_path
 
 davinci_path="${davinci_path//\'/}"
-unzip -f $davinci_path
-distrobox-enter --name drei -- sudo "${davinci_path//zip/run}" -iy
+current_path=$(pwd)
+unzip -o $davinci_path -d $current_path
+if [[ -e davinci.run ]]; then
+	rm -f davinci.run
+fi
+mv $current_path/*.run $current_path/davinci.run
+distrobox-enter --name drei -- sudo $current_path/davinci.run -iy
 distrobox-enter --name drei -- sudo rm -rf "/opt/resolve/libs/libglib-2.0.so"
 distrobox-enter --name drei -- sudo rm -rf "/opt/resolve/libs/libglib-2.0.so.0"
 distrobox-enter --name drei -- sudo rm -rf "/opt/resolve/libs/libglib-2.0.so.0.6800.4"
@@ -146,4 +183,8 @@ distrobox-enter --name drei -- sudo rm -rf "/opt/resolve/libs/libgmodule-2.0.so.
 distrobox-enter --name drei -- distrobox-export -a /opt/resolve/bin/resolve
 clear
 
-printf "\n\e[32;1m  L'installation s'est terminée avec succès !\e[0m\n\n"
+printf "\n
+	\e[42;1m                                               \e[0m
+	\e[42;1m  L'installation s'est terminée avec succès !  \e[0m
+	\e[42;1m                                               \e[0m
+\n\n"
